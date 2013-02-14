291b19cfe28548e44743152c7530ce22fb465d58
Daniel Bento <danielbento@overdestiny.com>
2013-02-14 16:09:41 +0000

    Init Interpolation

---
+++ video/interpolation.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+#
+# @file: interpolation.py
+# @date: 14-02-2013
+# @lastmodified: Thu 14 Feb 2013 04:09:08 PM WET
+#
+# @author:
+#
+# @description:
+#
+
+__author__ = ""
+__copyright__ = ""
+__credits__ = ""
+__licence__ = ""
+__version__ = ""
+__maintainer__ = ""
+__email__ = ""
+__status__ = ""
+
+from numpy import *
+from pylab import *
+import Image
+import cv
+import math
+
+def interpolate(p1,p2):
+    a = float(p1)
+    b = float(p2)
+    i = (a+b)/2
+    return [p1,int(i),p2]
+
+def main():
+    
+    capture = cv.CaptureFromFile("sem.mp4")
+    a = 0
+    while a < 100:
+        a = a + 1
+        img = cv.QueryFrame(capture)
+
+        tmp = cv.CreateImage(cv.GetSize(img),8,1)
+        cv.CvtColor(img, tmp, cv.CV_RGB2GRAY)
+        matrix = asarray(cv.GetMat(tmp))
+
+        result = zeros((len(matrix)*2-1,len(matrix[0])*2-1))
+    
+        i = 0
+        m = i
+        while i < len(matrix) and m < (len(result) - 2):
+            j = 0
+            k = j
+            while j < (len(matrix[i]) - 1) and k < (len(result[i])-2):
+                c = interpolate(matrix[i][j],matrix[i][j+1])
+                l = interpolate(matrix[i][j],matrix[i+1][j])
+                result[m][k] = c[0]
+                result[m][k+1] = c[1]
+                result[m][k+2] = c[2]
+                result[m][k] = l[0]
+                result[m+1][k] = l[1]
+                result[m+2][k] = l[2]
+                j = j + 1
+                k = k + 3
+            i = i + 1
+            m = m + 3
+        
+        img = Image.fromarray(result)
+        img.convert('L').save('%d.png' % a)
+
+if __name__ == "__main__":
+    main()
